724f03290c524896689093278db9f3947a994d98
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-01-22 19:14:44 +0700

    Restore ReviewHelpful model

---
+++ app/Models/ReviewHelpful.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReviewHelpful extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'review_id', 'user_id', 'is_helpful'
+    ];
+
+    // Relationships
+    public function review()
+    {
+        return $this->belongsTo(Review::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
