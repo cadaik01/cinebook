96d014882d321b7307922998ae94e85b888ca9df
TuanTu <tanghuynhtuantuwork@gmail.com>
2026-01-22 19:06:31 +0700

    Push all changes except .md files

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'TCA Cine')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     @vite([
-        'resources/css/app.css', 
+        'resources/css/app.css',
         'resources/css/header.css',
         'resources/css/footer.css',
         'resources/js/app.js'
